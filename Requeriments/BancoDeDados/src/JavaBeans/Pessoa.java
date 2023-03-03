137b3c7abf51f7d0d81428e635d21f79babeff2a
Luis Buscher <107158755+Luisbuscher@users.noreply.github.com>
2023-03-02 21:38:14 -0300

    Add files via upload

---
+++ Requeriments/BancoDeDados/src/JavaBeans/Pessoa.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaBeans;
+
+
+public class Pessoa {
+    String nome;
+    String telefone;
+    String celular;
+    String endereco;
+    String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }   
+    
+}
